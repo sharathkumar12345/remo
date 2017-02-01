54cb0a9d7752fb70900c70617f76a40ffc495fc6
sharathkumar12345 <kumbam.reddy@capgemini.com>
2017-02-01 18:22:46 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,66 @@
+resource "aws_vpc" "terraform" {
+     cidr_block = "10.100.0.0/16"
+}
+
+resource "aws_subnet" "public_1a" {
+    vpc_id = "${aws_vpc.terraform.id}"
+    cidr_block = "10.100.0.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "${var.availability_zone}"
+
+    tags {
+        Name = "Public 1"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.terraform.id}"
+
+    tags {
+        Name = "Terraform igw"
+    }
+}
+resource "aws_security_group" "Terraform_SG" {
+vpc_id = "${aws_vpc.terraform.id}"
+
+    tags {
+        Name = "Terraform igw"
+    }
+}
+resource "aws_security_group" "Terraform_SG" {
+  name = "Terraform_SG"
+  description = "SG allocation"
+  vpc_id = "${aws_vpc.terraform.id}"
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+ }
+egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web01" {
+    ami = "${var.aws_ami}"
+    instance_type = "${var.instance_type}"
+    subnet_id = "${aws_subnet.public_1a.id}"
+    vpc_security_group_ids = ["${aws_security_group.Terraform_SG.id}"]
+    key_name = "${var.key_name}"
+    tags {
+        Name = "terraform_launch"
+    }
+
+}
+                                   
---
+++ var.tf
@@ -0,0 +1,24 @@
+
+variable "aws_region" {
+        default = "us-west-2"
+}
+variable "availability_zone" {
+        default = "us-west-2a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-30e65350"
+}
+
+variable "key_name" {
+        default = "sharath"
+}
+
+variable "aws_access_key" {
+        default = "AKIAIUTA67ZUEQ2ZV7AA"
+}
+
